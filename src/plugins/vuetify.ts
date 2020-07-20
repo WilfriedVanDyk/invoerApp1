6c7bfd6b43f3cdb339a265429825b901fa3c1e07
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-07-20 12:42:08 +0200

    correcting typescript errors in src/plugin/vuetify.ts through adding declaring modules in shims-vue.d.ts

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,27 +1,28 @@
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-//import Vuetify from 'vuetify';
-import colors from 'vuetify/lib/util/colors.js';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdi'
+  },
   theme: {
     //does not work this time:  
     //success: colors.purple.darken1,
     //warning: colors.brown.lighten1,
-    icons: {
-      iconfont: 'mdi',
-    },
+    // icons: {
+    //   iconfont: 'mdi',
+    // },
     themes: {
       light: {
         primary: '#3f51b5',
         secondary: '#b0bec5',
         accent: '#8c9eff',
         error: '#b71c1c',
-        info: colors.grey.darken1,
-
+        info: colors.grey.lighten1
         //success: colors.purple.darken1,
         //warning: colors.brown.lighten1,
       },
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,11 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+declare module 'vuetify/lib' {
+  import 'vuetify/types/lib'
+}
+declare module 'vuetify/lib/util/colors.js' {
+  import 'vuetify/types/services/colors'
+}
+
+
